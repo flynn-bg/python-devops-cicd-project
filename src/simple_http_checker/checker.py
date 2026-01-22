68c7f785840c19190663be6e93038763e7219db4
Flynn Gardener <flynngardener@gmail.com>
2026-01-22 11:47:34 +0000

    feat(python-cicd): implement first version of workflow

---
+++ src/simple_http_checker/checker.py
@@ -0,0 +1,44 @@
+import logging
+import requests
+from typing import Collection
+
+logger = logging.getLogger(__name__)
+
+def check_urls(urls: Collection[str], timeout: int = 5) -> dict[str, str]:
+    """Checks a list of urls and returns status
+    
+    """
+
+    logger.info(f"Starting check for {len(urls)} URLs with a timeout of {timeout}")
+    results = {}
+
+    for url in urls:
+        logging.debug("zz9")
+        status = "UNKNOWN"
+
+        try:
+            logger.debug(f"Checking URL {url}")
+            response = requests.get(url, timeout=timeout)
+
+            if response.ok:
+                status = f"{response.status_code} OK"
+            else:
+                status = f"{response.status_code} {response.reason}"
+        except requests.exceptions.Timeout:
+            status = "TIMEOUT"
+            logger.warning(f"Request to {url} timed out.")
+        except requests.exceptions.ConnectionError:
+            status = "CONNECTION_ERROR"
+            logger.warning(f"Connection error for {url}.")
+        except requests.exceptions.RequestException as e:
+            status = f"REQUEST_ERROR: {type(e).__name__}"
+            logger.error(
+                f"An unexpected request error occured for {url}: {e}",
+                exc_info=True,
+            )
+        
+        results[url] = status
+        logger.debug(f"Checked: {url:<40} -> {status}")
+
+    logger.info("URL check finished.")
+    return results
---
+++ src/simple_http_checker/cli.py
@@ -0,0 +1,43 @@
+import logging
+import click
+
+from .checker import check_urls
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s] %(levelname)-8s %(name)s: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
+
+logger = logging.getLogger(__name__)
+
+@click.command()
+@click.argument("urls", nargs=-1)
+@click.option("--timeout", default=5, help="Timeout in seconds for each request.")
+@click.option("--verbose", "-v", is_flag=True, help="Enable debug logging")
+def main(urls, timeout, verbose):
+    if verbose:
+        logging.getLogger().setLevel(logging.DEBUG)
+        logger.debug("Verbose Logging Enabled")
+
+    logger.debug(f"Received urls: {urls}")
+    logger.debug(f"Received timeout: {timeout}")
+    logger.debug(f"Received verbose: {verbose}")
+    
+    if not urls:
+        logger.warning("NoURLs provided to check")
+        click.echo("Usages: chech-urls <URL1> <URL2> ..")
+        return
+    
+    logger.info(f"Starting check for {len(urls)}")
+
+    results = check_urls(urls, timeout)
+
+    click.echo("\n---Results---")
+    for url, status in results.items():
+        if "OK" in status:
+            click.secho(f"{url}:<40 -> {status}", fg="green")
+        else:
+            click.secho(f"{url}:<40 -> {status}", fg="red")
+
+
